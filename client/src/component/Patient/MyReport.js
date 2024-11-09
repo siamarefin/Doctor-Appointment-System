c202de57937c62e4578de038cb5b5c2fd0eac82b
Siam Arefin <siam12@student.sust.edu>
2024-11-09 23:43:36 +0600

    add database info

--- client/src/component/Patient/MyReport.js
+++ client/src/component/Patient/MyReport.js
@@ -1,37 +1,36 @@
 import React from "react";
 import { useDataContext } from "@/context/DataContext";
+import "./style.css"; // Import your style.css file
 
 function MyReport() {
   const { myReports } = useDataContext();
 
   return (
-    <div className="h-full flex flex-col rounded-lg overflow-hidden">
-      <table className="w-full table-fixed border-collapse">
-        <thead className=" text-black sticky top-0">
-          <tr >
-            <th className="border  px-4 py-2 text-start w-[40%]">Test Name</th>
-            <th className="border px-4 py-2 text-start">Result</th>
-            <th className="border px-4 py-2 text-start">Amount</th>
-            <th className="border px-4 py-2 text-start">Comment</th>
+    <div className="table-container"> {/* Use the general table container class */}
+      <table className="table"> {/* Apply the .table class for styling */}
+        <thead>
+          <tr>
+            <th className="w-[40%]">Test Name</th>
+            <th>Result</th>
+            <th>Amount</th>
+            <th>Comment</th>
           </tr>
         </thead>
-        <tbody className="bg-white">
-          {myReports.map((report, index) => (
-            <tr key={index} className="text-black text-center">
-              <td className="border px-4 py-2 text-start w-[40%] overflow-hidden whitespace-nowrap text-ellipsis">
-                {report.test_name}
-              </td>
-              <td className="border px-4 py-2 text-start overflow-hidden whitespace-nowrap text-ellipsis">
-                {report.result}
-              </td>
-              <td className="border px-4 py-2 text-start overflow-hidden whitespace-nowrap text-ellipsis">
-                {report.amount}
-              </td>
-              <td className="border px-4 py-2 text-start overflow-hidden whitespace-nowrap text-ellipsis">
-                {report.comment}
-              </td>
+        <tbody>
+          {myReports.length > 0 ? (
+            myReports.map((report, index) => (
+              <tr key={index} className="text-center">
+                <td className="overflow-hidden">{report.test_name}</td>
+                <td className="overflow-hidden">{report.result}</td>
+                <td className="overflow-hidden">{report.amount}</td>
+                <td className="overflow-hidden">{report.comment}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="4" className="no-data">No data available</td>
             </tr>
-          ))}
+          )}
         </tbody>
       </table>
     </div>
